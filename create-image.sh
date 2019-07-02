892823d35e1850ee1ac64fd9c01790ff16f72dcd
Niko Böckerman <niko.bockerman@iki.fi>
2019-07-02 21:09:22 +0300

    fixup! Add own scripts

--- create-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cur_dir=$(dirname "$(readlink -f "$0")")
-
-sudo docker build -t ruuvi-collector:$(<"$cur_dir"/configuration-description.txt) .
-
-echo "Created new image, stop old container, rename it, start new container with 'start-collector.sh'."
--- update-base-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-sudo docker pull maven:3.6-jdk-8-alpine
-
-echo "Updated maven base image. Update dependencies in pom.xml, build new image with 'create-image.sh'."
